908eaa87ac577838503845f2c8b43cc5937c69c7
Jon <jonrose08@gmail.com>
2012-05-20 16:12:18 -0700

    updates project.clj to include clooj dep

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject overtone-sketchpad "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A light weight IDE for programming with Overtone"
+  :main overtone-sketchpad.core
   :dependencies [[org.clojure/clojure "1.3.0"]
-  				 [overtone "0.6.0"]])
+  				 [overtone "0.6.0"]
+  				 [clooj "0.3.4.1-SNAPSHOT"]])
