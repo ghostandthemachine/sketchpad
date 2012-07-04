37bd2bf722c64dd7aedf9ccf04132c46c0ef3bb8
Jon <jonrose08@gmail.com>
2012-07-03 21:55:29 -0700

    merge dev file structure updates

--- src/sketchpad/main_background.clj
+++
@@ -1,7 +0,0 @@
-(ns sketchpad.main-background
-	(:use [seesaw.core])
-	(:import (javax.swing ImageIcon)))
-
-
-; (defn background-comp []
-; 	(border-panel :center (icon (ImageIcon. "img/Clojure-glyph-grayscale.png"))))
