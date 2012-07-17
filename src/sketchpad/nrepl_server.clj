09a7504bcbe10d6f9657243f2f7f3735fb6cfc1f
Jon <jonrose08@gmail.com>
2012-07-16 22:17:29 -0700

    remove nrepl-server file

--- src/sketchpad/nrepl_server.clj
+++
@@ -1,6 +0,0 @@
-(ns sketchpad.nrepl-server
-	(:use [clojure.tools.nrepl.server :only (start-server stop-server)]))
-(defonce port (first *command-line-args*))
-(spit "log.txt" "running nrepl script...")
-(start-server :port port)
-(spit "log.txt" (str "Started nREPL server on port: " port))
