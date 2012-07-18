6a7da3e1ab12836165cae36566470c728975892b
Jeff Rose <jeff@deepmind.com>
2012-07-18 16:35:20 +0100

    Project repls at last!
    Just needed to add the nrepl dependency to the project so it was available to
    start inside the new JVM, and then quote the require correctly.
    
    I am able to (use 'overtone.live) and make a sound, which is great, but now it seems
    the problem is with some of the connection machinery that is taking the output
    from the other processes and printing it.

--- src/sketchpad/lein_manager.clj
+++ src/sketchpad/lein_manager.clj
@@ -58,14 +58,16 @@
                    (-> project :repl-options :ack-port))]
     (Integer. p)))
 
-
+; TODO: The tools.nrepl dependency should not be hard coded here.  Instead it should be setup
+; to extract it from the leiningen version currently being used, so that we use the same
+; nrepl lein has been written to work with.
 (defn project-repl-server [project]
-	(let [port (next-repl-port!)]
-;				project (dissoc :prep-tasks)]
+	(let [port (next-repl-port!)
+          project (update-in project [:dependencies] #(conj % '[org.clojure/tools.nrepl "0.2.0-beta8"]))]
 		(.start
 			(Thread.
 				(bound-fn []
 					(eval/eval-in-project project
                            `(nrepl.server/start-server :port ~port)
-                           '(require [clojure.tools.nrepl.server :as nrepl.server])))))
+                           `(require '[clojure.tools.nrepl.server :as nrepl.server])))))
 		{:port port}))
