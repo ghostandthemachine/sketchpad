cb8ac677ac15390832c5fc83b0e75ea19dc0fe31
jon <jonrose08@gmail.com>
2012-08-07 08:46:25 -0700

    add mini-map.clj

---
+++ src/sketchpad/editor/mini_map.clj
@@ -0,0 +1,12 @@
+(ns sketchpad.editor.mini-map
+	(:require [seesaw.core :as seesaw]
+						[seesaw.rsyntax :as rsyntax]))
+
+
+(defn mini-map 
+"Created a mini map component for a given text area."
+	[rsta]
+	(let [mini-rsta (rsyntax/text-area
+											:class :mini-map
+											:syntax (config rsta :syntax)
+											)]))
